dce2b9f6300a073bc06f0396fca6c0d43f63728a
qingxp9-360 <qingxp9@gmail.com>
2015-09-14 18:51:41 +0800

    add home page 'We are pegasus' and change size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  #http_basic_authenticate_with  password: "1"
   protect_from_forgery with: :exception
 end
