72de8ce865bac5ad567a18f7201c257906dc0741
qingxp9-360 <qingxp9@gmail.com>
2015-09-17 17:20:51 +0800

    zmapworker

--- app/controllers/cpanel/zmap_controller.rb
+++ app/controllers/cpanel/zmap_controller.rb
@@ -5,18 +5,13 @@ module Cpanel
 
     def scan
       target_name = params[:name]? params[:name] : "outer"
-      target_ip   = params[:ip].split(",")
+      target_ip   = params[:ip].sub(","," ")
       ports       = params[:port].split(",")
-      #ZmapWorkerJob.perform_later(
-        #target_name, target_ip, ports
-      #)
-      #target_ip = ["10.18.25.0/24"]
-      #ports = [ 22,80 ]
 
+      ZmapWorkerJob.perform_later(
+        target_name, target_ip, ports
+      )
 
-      #ZmapWorkerJob.perform_now(
-        #target_name, target_ip, ports
-      #)
       render inline: "#{target_name} <br/> #{target_ip} <br/> #{ports}  "
 
     end
