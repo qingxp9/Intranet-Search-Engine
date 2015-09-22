adfbe3ffd9147ffb135a4ac90d84d19d56506c80
qingxp9-360 <qingxp9@gmail.com>
2015-09-22 16:52:34 +0800

    separate shared/host minisearch sidebar

--- app/controllers/intranets_controller.rb
+++ app/controllers/intranets_controller.rb
@@ -2,14 +2,10 @@ class IntranetsController < ApplicationController
 
   def index
     @intranets = Intranet.page(params[:page]).per(10)
-    begin_time = Time.now
-    @waste_time = Time.now - begin_time
   end
 
   def search
-    begin_time = Time.now
     @intranets = Intranet.search(params[:q]).records.page(params[:page]).per(30)
-    @waste_time = Time.now - begin_time
     render 'index'
   end
 
--- app/views/hosts/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@websites) do |website|
-  json.extract! website, :id, :target, :port, :webapp, :server, :os, :headers, :title, :keywords, :description, :body, :check_time
-  json.url website_url(website, format: :json)
-end
